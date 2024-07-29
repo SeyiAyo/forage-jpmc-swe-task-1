e94ac7197cab9eacc3d39068035773fe8e17b72e
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2024-07-29 22:52:56 +0100

    data feed fixed

--- client_test.py
+++ client_test.py
@@ -8,6 +8,14 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      expected_price = (bid_price + ask_price) / 2
+      self.assertEqual(price, expected_price)
+      self.assertEqual(stock, quote['stock'])
+      self.assertEqual(bid_price, float(quote['top_bid']['price']))
+      self.assertEqual(ask_price, float(quote['top_ask']['price']))
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +23,21 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      stock, bid_price, ask_price, price = getDataPoint(quote)
+      expected_price = (bid_price + ask_price) / 2
+      self.assertEqual(price, expected_price)
+      self.assertEqual(stock, quote['stock'])
+      self.assertEqual(bid_price, float(quote['top_bid']['price']))
+      self.assertEqual(ask_price, float(quote['top_ask']['price']))
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_getRatio(self):
+    from client3 import getRatio
+    self.assertEqual(getRatio(120.48, 119.2), 120.48 / 119.2)
+    self.assertIsNone(getRatio(120.48, 0))
+    self.assertEqual(getRatio(0, 119.2), 0)
+    self.assertEqual(getRatio(119.2, 119.2), 1)
 
 
 if __name__ == '__main__':
